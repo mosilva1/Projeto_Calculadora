eaa36959d0b21c470187891a0d85b715011c2b3c
mosilva1 <mateusdeoliveirasilvas@gmail.com>
2022-03-30 12:22:09 -0300

    Js

--- javascript-projeto-calculadora-clone-master/scripts/controller/CalcController.js
+++ javascript-projeto-calculadora-clone-master/scripts/controller/CalcController.js
@@ -1,4 +1,16 @@
 //classe começa com letra maiúscula
 class CalController{
-
+    constructor(){
+        this._displayCalc = "0";
+        this._dataAtual;
+    }
+    geDisplay(){
+        return this._displayCalc;
+    }
+    set _displayCalc(valor){
+        this._displayCalc=valor;
+    }
 }
+/* 3 tipos de encapsulamento:
+public - protect - privat
+*/
